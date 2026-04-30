bb37d18ebab3ed41a41d0e09a49ee354afdeb85e
CristianoDevRocket <cristianofs2013@gmail.com>
2026-04-30 03:41:20 -0300

    feat: define constantes do domínio

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const API_BASE_URL = 'http://localhost:3001';
+export const PRODUTOS_ENDPOINT = '/produtos';
+
+export const ARMAZENS = [
+  'Centro Logístico 1',
+  'Depósito Central',
+  'Hub Zona Sul',
+  'Armazém A',
+  'Armazém B',
+];
